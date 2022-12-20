4cc73361e939e2638cdc1902007685829f5d6505
Guilherme Cardoso <cardosoghr@gmail.com>
2022-12-20 19:21:43 -0300

    addind register page

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+  } from "react-router-dom";
+  import { Feed } from "./pages/feed";
+  
+  import { Home } from './pages/home'
+  import { Login } from './pages/login'
+  import { Register } from './pages/register'
+  import { GlobalStyle } from './styles/global';
+  
+  function App() {
+    return (
+      <Router>
+       <GlobalStyle />
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/register" element={<Register />} />
+          
+       </Routes >
+      </Router>
+    );
+  }
+  
+  export default App;
